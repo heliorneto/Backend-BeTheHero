ae436d0631a82657fe6db733c1c81afc6a068f22
Hélio Neto <helioneto29@hotmail.com>
2022-02-19 14:29:07 -0300

    Criando sessão de login

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,8 @@ const routes = express.Router();
 
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 
 // Ongs
 routes.get('/ongs', OngController.index);
@@ -12,5 +14,11 @@ routes.post('/ongs', OngController.create);
 routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
 routes.delete('/incidents/:id', IncidentController.delete);
+
+// Profile
+routes.get('/profile', ProfileController.index);
+
+// Session
+routes.post('/sessions', SessionController.create);
 
 module.exports = routes;
