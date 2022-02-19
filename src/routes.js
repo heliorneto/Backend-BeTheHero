99a89601a22187695396ab2dbe7c838440927ba1
Hélio Neto <helioneto29@hotmail.com>
2022-02-19 13:43:13 -0300

    Criando endpoint para deletar caso

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,15 @@ const express = require('express');
 const routes = express.Router();
 
 const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
 
+// Ongs
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
+
+// Incidents
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
 
 module.exports = routes;
